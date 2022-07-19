c2ae3a56e7e1db613aabd980ae1a36fbbaedc06e
sixdigit <mog@sixdigit.net>
2022-07-19 15:29:54 -0400

    Selenium

--- RegEx/UtilityAPI/Guangzhuan(Morris)_parse_html.py
+++ RegEx/UtilityAPI/Guangzhuan(Morris)_parse_html.py
@@ -4,13 +4,18 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 import re
 
-def parse_html(file=''):
-    base_dir = Path(__file__).parent
-    file = os.path.join(base_dir, file)
 
+def parse_html(file=''):
     with open(file, 'r', encoding='utf8') as f:
         soup = BeautifulSoup(f, 'html.parser')
+    return soup
+
+
+def html_parser(soup=''):
+    return res
 
 
 if __name__ == "__main__":
-    parse_html('')
+    file = os.path.join(Path(__file__).parent, '')
+    txt = parse_html(file)
+    res = html_parser(txt)
