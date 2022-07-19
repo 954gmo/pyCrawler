efc501f2bd129c069bede1dc5ce4e94cd1ab2e42
sixdigit <mog@sixdigit.net>
2022-07-19 16:37:20 -0400

    Selenium

--- RegEx/UtilityAPI/Guangzhuan(Morris)_parse_html.py
+++ RegEx/UtilityAPI/Guangzhuan(Morris)_parse_html.py
@@ -9,11 +9,8 @@ if __name__ == "__main__":
     with open(file, 'r', encoding='utf8') as f:
         soup = BeautifulSoup(f, 'html.parser')
         service_addr = soup.find('div', {'class': 'module-body sa-info'}).find('ul').find_all('li')[:2]
-        print('service address: ')
-        print(re.sub(r'\s+', ' ', service_addr[0].string) + "," + re.sub(r'\s+', ' ', service_addr[1].string))
-
+        service_addr = re.sub(r'\s+', ' ', service_addr[0].string) + "," + re.sub(r'\s+', ' ', service_addr[1].string)
         rate_name = soup.find('div', {'class': 'module module-grad my-rate-plan'}).find('h3').string
-        print('rate name: ')
-        print(re.sub(r'\s+', ' ', rate_name))
-
-
+        rate_name = re.sub(r'\s+', ' ', rate_name)
+        print(f'service address: {service_addr}')
+        print(f'rate name: {rate_name}')
