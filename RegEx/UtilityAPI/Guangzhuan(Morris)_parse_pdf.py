8a8158c68049bfd06a24fc62301ade2cd4d2edee
sixdigit <mog@sixdigit.net>
2022-07-19 15:15:18 -0400

    Selenium

--- RegEx/UtilityAPI/Guangzhuan(Morris)_parse_pdf.py
+++ RegEx/UtilityAPI/Guangzhuan(Morris)_parse_pdf.py
@@ -4,18 +4,19 @@ from pathlib import Path
 import os
 
 
-def parse_pdf(file=''):
+def read_pdf_txt(file=''):
     with open(file, 'rb') as pdf_file_obj:
         reader = PyPDF2.PdfFileReader(pdf_file_obj)
-        # # reader.decrypt('')
-        # page = reader.getPage(0)
-        # contents = page.getContents()
-        # print(page.extract_text())
         text = ''
         for page in reader.pages:
             text += page.extractText()
-        print(text)
+    return txt
+
+
+def pdf_parser(content=''):
+    pass
 
 
 if __name__ == "__main__":
-    parse_pdf(file=os.path.join(Path(__file__).parent, 'PembrokePinesFL UB Bill PDF.pdf'))
+    txt = read_pdf_txt(file=os.path.join(Path(__file__).parent, 'PembrokePinesFL UB Bill PDF.pdf'))
+    pdf_parser(txt)
