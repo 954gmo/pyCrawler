b48d8c27a90490a5bf0983f97a93c8666cc78a3a
sixdigit <mog@sixdigit.net>
2022-07-19 16:34:31 -0400

    Selenium

--- RegEx/UtilityAPI/Guangzhuan(Morris)_parse_pdf.py
+++ RegEx/UtilityAPI/Guangzhuan(Morris)_parse_pdf.py
@@ -4,23 +4,17 @@ from pathlib import Path
 import os
 import re
 
-
-def read_pdf_txt(file=''):
+if __name__ == "__main__":
+    file = os.path.join(Path(__file__).parent, 'sdge_bill.pdf')
     with open(file, 'rb') as pdf_file_obj:
         reader = PyPDF2.PdfFileReader(pdf_file_obj)
         text = ''
         for page in reader.pages:
             text += page.extractText()
-    return text
+    tmp = re.sub(r'\s+', ' ', text)
+    tmp = re.search(r"Electric Service.*(Meter Number:.*?)(\d+).*Total Taxes & Fees on Electric Charges .*\$(\d+\.?\d+).*?Total Electric Service", tmp)
+    print('Meter Number:')
+    print(tmp[2])
+    print('Total Taxes & Fees on Electric Charges:')
+    print('-' + tmp[3])
 
-
-def pdf_parser(content=''):
-    res = content
-    return res
-
-
-if __name__ == "__main__":
-    file = os.path.join(Path(__file__).parent, 'sdge_bill.pdf')
-    txt = read_pdf_txt(file=file)
-    res = pdf_parser(txt)
-    print(res)
